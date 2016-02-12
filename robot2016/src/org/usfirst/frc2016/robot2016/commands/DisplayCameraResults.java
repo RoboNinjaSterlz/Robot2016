6ec4b67452fa1ae8ee7aa844e07b07ab32aa3515
team2016 <team2016@74d4bbe2-dc28-4520-bb23-51a49fec9215>
2016-02-12 02:15:26 +0000

    Moved code to init from execute

--- robot2016/src/org/usfirst/frc2016/robot2016/commands/DisplayCameraResults.java
+++ robot2016/src/org/usfirst/frc2016/robot2016/commands/DisplayCameraResults.java
@@ -38,19 +38,21 @@ public class DisplayCameraResults extends Command {
 
     // Called just before this Command runs the first time
     protected void initialize() {
+    	Robot.vision.loadLines();
+    	Robot.vision.displayLines();
+    	Robot.vision.loadContours();
+    	Robot.vision.displayContours();
     	//Robot.vision.loadContours();
     	//Robot.vision.displayContours();
     }
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	Robot.vision.loadLines();
-    	Robot.vision.displayLines();
     }
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return false;
+        return true;
     }
 
     // Called once after isFinished returns true
